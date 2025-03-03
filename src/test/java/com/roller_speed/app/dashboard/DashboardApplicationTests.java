73d0f044de3f8285adf96469daa13ed799bde029
Luis Toro <85536600+luthors@users.noreply.github.com>
2025-03-02 22:19:19 -0500

    Initial commit

---
+++ src/test/java/com/roller_speed/app/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roller_speed.app.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
